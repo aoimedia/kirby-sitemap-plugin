b09f3d4b459d51feb8874865c7ab825d941fc6bd
Sebastian Eberlein <SebastianEberlein@users.noreply.github.com>
2016-10-20 12:08:46 +0200

    Improve indentation

--- sitemap.php
+++ sitemap.php
@@ -1,42 +1,43 @@
 <?php
 
 kirby()->routes(array(
-	array(
-		'pattern' => 'sitemap',
-		'action'  => function() {
-			return go('sitemap.xml');
-		}
-	),
-	array(
-		'pattern' => 'sitemap.xml',
-		'action'  => function() {
-			$sitemap = '<?xml version="1.0" encoding="utf-8"?>'. "\n" .'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">'. "\n";
+  array(
+    'pattern' => 'sitemap',
+    'action'  => function() {
+      return go('sitemap.xml');
+    }
+  ),
+  array(
+    'pattern' => 'sitemap.xml',
+    'action'  => function() {
 
-			$excluded = kirby()->site()->pages()->index()->filterBy('template', 'in', ['error']);
-			$pages = kirby()->site()->pages()->index()->not($excluded);
+      $sitemap = '<?xml version="1.0" encoding="utf-8"?>'. "\n" .'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">'. "\n";
 
-			foreach($pages as $p) {
-				$sitemap .= '<url>' . "\n";
-				$sitemap .= '<loc>' . html($p->url()) . '</loc>' . "\n";
+      $excluded = kirby()->site()->pages()->index()->filterBy('template', 'in', ['error']);
+      $pages = kirby()->site()->pages()->index()->not($excluded);
 
-				if (kirby()->site()->languages() && kirby()->site()->languages()->count() > 1) {
-					foreach (kirby()->site()->languages() as $language) {
-						$code = $language->code();
-						if ($code != kirby()->site()->defaultLanguage()->code()) {
-							$sitemap .= '<xhtml:link rel="alternate" hreflang="' . $code . '" href="' . $p->url($code) . '" />' . "\n";
-						}
-					}
-				}
+      foreach($pages as $p) {
+        $sitemap .= '<url>' . "\n";
+        $sitemap .= '<loc>' . html($p->url()) . '</loc>' . "\n";
 
-				$modified = c::get('date.handler') === 'strftime' ? strftime('%F', $p->modified()) : $p->modified('Y-m-d');
-				$sitemap .= '<lastmod>' . $modified . '</lastmod>' . "\n";
-				$priority = $p->isHomePage() ? 1 : 0.8 / $p->depth();
-				$sitemap .= '<priority>' . $priority . '</priority>' . "\n";
-				$sitemap .= '</url>' . "\n";
-			}
-			$sitemap .= '</urlset>';
+        if (kirby()->site()->languages() && kirby()->site()->languages()->count() > 1) {
+          foreach (kirby()->site()->languages() as $language) {
+            $code = $language->code();
+            if ($code != kirby()->site()->defaultLanguage()->code()) {
+              $sitemap .= '<xhtml:link rel="alternate" hreflang="' . $code . '" href="' . $p->url($code) . '" />' . "\n";
+            }
+          }
+        }
 
-			return new Response($sitemap, 'xml');
-		}
-	)
+        $modified = c::get('date.handler') === 'strftime' ? strftime('%F', $p->modified()) : $p->modified('Y-m-d');
+        $sitemap .= '<lastmod>' . $modified . '</lastmod>' . "\n";
+        $priority = $p->isHomePage() ? 1 : 0.8 / $p->depth();
+        $sitemap .= '<priority>' . $priority . '</priority>' . "\n";
+        $sitemap .= '</url>' . "\n";
+      }
+      $sitemap .= '</urlset>';
+
+      return new Response($sitemap, 'xml');
+    }
+  )
 ));
